a6dde08643b757bf63e901fdcb7dc7eaf79ecf94
Enes <enesalp_aslan@outlook.com>
2025-03-02 23:18:56 +0300

    add priority

--- Week2/hw4/script.js
+++ Week2/hw4/script.js
@@ -1,50 +1,43 @@
-const input=document.querySelector("#input-box")
-const listContainer=document.querySelector("#list-container")
+const input = document.querySelector("#task-name");
+const listContainer = document.querySelector("#list-container");
+const taskImportance = document.querySelector("#task-importance");
+let data = {};
 
+const addTask = () => {
+  saveData();
+};
 
-const addTask = ()=>{
-
-    if(input.value===""){
-        alert('Task can not be blank')
-    }else{
-
-        let li =document.createElement("li")
-        li.innerHTML=input.value
-        listContainer.appendChild(li)
-        input.value=""
-        let span=document.createElement("span")
-        span.innerHTML="\u00d7"
-        li.appendChild(span)
-        saveData()
-
-
-
-
-
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if ((e.target.tagName = "SPAN")) {
+      e.target.parentElement.remove();
     }
+    saveData();
+  },
+  false
+);
 
-}
+const saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
 
+const showTasks = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
 
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName==="LI"){
-      
-        e.target.classList.toggle("checked")
-    }else if(e.target.tagName="SPAN"){
-       
-        e.target.parentElement.remove()
+const getSelectedImportance = () => {
+  const radios = document.getElementsByName("task-importance-level");
+  let selectedValue = "";
+  for (const radio of radios) {
+    if (radio.checked) {
+      selectedValue = radio.value;
+      break;
     }
-    saveData()
-},false)
+  }
+  return selectedValue;
+};
 
-
-const saveData=()=>{
-
-    localStorage.setItem("data",listContainer.innerHTML)
-}
-
-const showTasks=()=>{
-    listContainer.innerHTML=localStorage.getItem("data")
-}
-
-showTasks()
+showTasks();
