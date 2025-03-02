5b5e15955d2effa515363b00f14cabc5d0163241
Enes <enesalp_aslan@outlook.com>
2025-03-03 01:46:28 +0300

    fix importance

--- Week2/hw4/script.js
+++ Week2/hw4/script.js
@@ -1,31 +1,43 @@
 const input = document.querySelector("#task-name");
 const listContainer = document.querySelector("#list-container");
 const taskImportance = document.querySelector("#task-importance");
-let data = {};
+const storageName = "task-data-storage";
+let data = JSON.parse(localStorage.getItem(storageName)) ?? [];
 
 const addTask = () => {
+  const newTask = {
+    name: input.value,
+    importance: getSelectedImportance(),
+  }
+
+  data.push(newTask)
+
   saveData();
 };
 
-listContainer.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
-    } else if ((e.target.tagName = "SPAN")) {
-      e.target.parentElement.remove();
-    }
-    saveData();
-  },
-  false
-);
-
 const saveData = () => {
-  localStorage.setItem("data", listContainer.innerHTML);
+  localStorage.setItem(storageName, JSON.stringify(data));
 };
 
 const showTasks = () => {
-  listContainer.innerHTML = localStorage.getItem("data");
+  const tasksHTML = data.map((task) => {
+    const currentTaskName = task.name;
+    const currentTaskDescription = task.description;
+    const currentTaskStatus = task.status;
+    const currentTaskImportance = task.importance;
+
+    const currentTaskHTML =
+      `<li class="task-${currentTaskImportance} task">
+        <input type="checkbox" class="task-importance-checkbox"/>
+        <p class="task-name">${currentTaskName}</p>
+        <div class="task-action-buttons">
+          <span class="remove">X</span>
+          <span class="remove">&#9660;</span>
+        </div>
+      </li>`;
+
+    listContainer.insertAdjacentHTML('beforeend', currentTaskHTML);
+  })
 };
 
 const getSelectedImportance = () => {
@@ -41,3 +53,26 @@ const getSelectedImportance = () => {
 };
 
 showTasks();
+
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "INPUT") {
+      e.target.parentElement.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      if (window.confirm('Bu taskı silmeyi onaylıyor musunuz?')) {
+        const li = e.target.closest('li')
+        const ul = li.parentElement;
+        const index = Array.from(ul.children).indexOf(li);
+
+        data.splice(index, 1);
+
+        e.target.closest('.task').remove();
+      }
+    }
+
+    saveData();
+  },
+  false
+);
