dc34a109b1953eda93d1d0da6aa7b2166fa6a5a7
Enes <enesalp_aslan@outlook.com>
2025-03-05 20:06:37 +0300

    add delay

--- Week3/hw2/script.js
+++ Week3/hw2/script.js
@@ -1,62 +1,49 @@
-$(()=>{
+$(() => {
+  let currentPage = 0;
+  const limit = 7;
+  let isLoading = false;
 
-let currentPage=0;
-const limit=7
-let isLoading=false;
-// let cachedPosts={}
-
-
-// function loadMore()
-// {
-//    alert("More loaded");
-//    $(window).bind('scroll');
-//  }
-
-$(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-    if(!isLoading){
-        loadPost();
-
-    }
-   }
-});
-
-const loadPost=()=>{
-    isLoading=true;
-    $('#loading').show();
-    $.getJSON('https://jsonplaceholder.typicode.com/posts',{
-        _start:currentPage * limit,
-        _limit:limit
+  const loadPost = () => {
+    isLoading = true;
+    $(".loading").show();
+    $.getJSON("https://jsonplaceholder.typicode.com/posts", {
+      _start: currentPage * limit,
+      _limit: limit,
     })
-        .done(posts=>{
-            posts.forEach(post => {
-                const html=`
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title
-                    ">${post.title}</h5>
-                    <p class="card-text">${post.body}</p>
-                </div>
-                </div>
-                `;
-                $('#postList').append(html);
-                
-        
+      .done((posts) => {
+        setTimeout(() => {
+          isLoading = false;
+          $(".loading").hide();
+          $.each(posts, (index, post) => {
+            const html = `
+                              <div class="card">
+                              <div class="card-body">
+                                  <h5 class="card-title">${post.title}</h5> 
+                                  <p class="card-text">${post.body}</p>
+                              </div>
+                              </div>
+                              `;
+            $(".postList").append(html);
+          });
+        }, 2000);
+
+        currentPage++;
+      })
+      .fail((error) => {
+        console.log(error);
+      })
+      .always(() => {});
+  };
+  loadPost();
+
+  $(window).scroll(function () {
+    if (
+      $(window).scrollTop() + $(window).height() >
+      $(document).height() - 100
+    ) {
+      if (!isLoading) {
+        loadPost();
+      }
     }
-);
-    currentPage++;
-   }
-  
-)
-   .fail(error=>{
-       console.log(error);
-   })
-   .always(()=>{
-     isLoading=false;
-     $('#loading').hide();
-    });
-}
-
-loadPost();
-
-})
+  });
+});
